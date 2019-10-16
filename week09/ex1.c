e3df56f61a5b55b73deb0d693c8688a6c6dcff6e
Mike <super.mvk@yandex.ru>
2019-10-16 19:23:37 +0300

    Temp implementation, new sample files added

--- week09/ex1.c
+++ week09/ex1.c
@@ -11,25 +11,21 @@ typedef struct {
 } page_t;
 
 const unsigned int INF = -1;
-const unsigned int RBit = 1u<<(sizeof(int)*8-1);
+const unsigned int RBit = 1u << (sizeof(int) * 8 - 1);
 int PAGE_AMOUNT = 0;
 int hits = 0;
 int misses = 0;
-FILE* inputFile = NULL;
+FILE *inputFile = NULL;
 
 // Lookup through the table and update counters
 // Returns 1 if page have hit and 0 otherwise
-char lookup(int pageid, page_t table[]){
+char lookup(int pageid, page_t table[]) {
     char success = 0;
 
-    for(int i = 0; i < PAGE_AMOUNT; ++i){
+    for (int i = 0; i < PAGE_AMOUNT; ++i) {
         table[i].counter >>= 1u;
-        if(table[i].id == -1){
-            table[i].id = pageid;
-            table[i].counter = RBit;
-            success = 1;
-            break;
-        }else if(table[i].id == pageid){
+
+        if (table[i].id == pageid) {
             table[i].counter |= RBit;
             success = 1;
         }
@@ -38,16 +34,17 @@ char lookup(int pageid, page_t table[]){
     return success;
 }
 
-void insert_page(int pageid, page_t table[]){
-    unsigned int mindex = -1, mcount = INF;
-    for(int i = 0; i < PAGE_AMOUNT; ++i){
-        if(table[i].id == -1){
+void insert_page(int pageid, page_t table[]) {
+    unsigned int mindex = -1, mcounter = INF;
+    for (int i = 0; i < PAGE_AMOUNT; ++i) {
+        if (table[i].id == -1) {
             table[i].id = pageid;
             table[i].counter = RBit;
+            return;
         }
-        if(table[i].counter < mcount){
-            mindex = table[i].id;
-            mcount = table[i].counter;
+        if (table[i].counter < mcounter) {
+            mindex = i;
+            mcounter = table[i].counter;
         }
     }
 
@@ -58,44 +55,46 @@ void insert_page(int pageid, page_t table[]){
 
 // Helper functions
 // Return next referencing page number from the file
-int next_page(){
+int next_page() {
     int buff = 0;
-    if(fscanf(inputFile, "%d", &buff) == 1){
+    if (fscanf(inputFile, "%d", &buff) == 1) {
         return buff;
     }
     return -1;
 }
 
 // Initialize page table with zero values
-void initialize_table(page_t table[]){
-    for(int i = 0; i < PAGE_AMOUNT; ++i){
-        table[i].id=-1;
+void initialize_table(page_t table[]) {
+    for (int i = 0; i < PAGE_AMOUNT; ++i) {
+        table[i].id = -1;
         table[i].counter = 0;
     }
 }
 
 // Print to 'stdout' usage message
-void print_usage(){
+void print_usage() {
     printf("Usage: ./ex1 number file\nwhere\n");
     printf("\tnumber - is a amount of page frames\n");
     printf("\tfile - is a path to input file\n");
 }
 
-int main(int argc, char* argv[]) {
-    if(argc != 3){
+int main(int argc, char *argv[]) {
+    /*
+    if (argc != 3) {
         print_usage();
         exit(0);
     }
 
-    PAGE_AMOUNT = (int)strtol(argv[1], NULL, 10);
+    PAGE_AMOUNT = (int) strtol(argv[1], NULL, 10);
 
-    if(PAGE_AMOUNT < 1){
+    if (PAGE_AMOUNT < 1) {
         printf("Incorrect 'number' specified\nExiting...");
         exit(0);
-    }
+    }*/
 
-    inputFile = fopen(argv[2], "r");
-    if(inputFile == NULL){
+    PAGE_AMOUNT = 3;
+    inputFile = fopen("../week09/input4.txt", "r");
+    if (inputFile == NULL) {
         printf("Failed to open specified 'file'\nExiting...");
         exit(0);
     }
@@ -106,11 +105,11 @@ int main(int argc, char* argv[]) {
     // Index of the referencing page
     int ref_index = -1;
 
-    while((ref_index = next_page()) != -1){
+    while ((ref_index = next_page()) != -1) {
 
-        if(lookup(ref_index, memtable) == 1){
+        if (lookup(ref_index, memtable)) {
             hits++;
-        }else{
+        } else {
             misses++;
             insert_page(ref_index, memtable);
         }
@@ -118,7 +117,7 @@ int main(int argc, char* argv[]) {
 
     printf("%d page frames\n", PAGE_AMOUNT);
     printf("    %d hits, %d misses\n", hits, misses);
-    printf("    Hit/Miss ration is: %lf\n", (double)hits/misses);
+    printf("    Hit/Miss ration is: %lf\n", (double) hits / misses);
 
     return (0);
 }
